f17d03430bf612b01fb26a8dd3a00cd99eef76cc
mumu2117 <jmillerarcher@me.com>
2020-08-15 12:40:36 -0400

    commiti channges

--- src/helpers/colorContrast.js
+++ src/helpers/colorContrast.js
@@ -1,4 +1,3 @@
-// function from https://stackoverflow.com/questions/35969656/how-can-i-generate-the-opposite-color-according-to-current-color
 export default function colorContrast(color) {
   let hex = color;
   if (hex.indexOf('#') === 0) {
@@ -14,6 +13,5 @@ export default function colorContrast(color) {
   const r = parseInt(hex.slice(0, 2), 16);
   const g = parseInt(hex.slice(2, 4), 16);
   const b = parseInt(hex.slice(4, 6), 16);
-  // http://stackoverflow.com/a/3943023/112731
   return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? '#333' : '#fff';
 }
